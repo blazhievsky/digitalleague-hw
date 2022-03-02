8e57113f371220ece38db668f89409aa1086a2ac
blazhievsky <blazhievsky007@ya.ru>
2022-03-02 16:25:28 +0300

    Добавил папку 5-го дня

--- Day_3/emailCard/ClassWork/ViewController (2).swift
+++ Day_3/emailCard/ClassWork/ViewController (2).swift
@@ -2,8 +2,9 @@
 //  ViewController.swift
 //  ClassWork
 //
-//  Created by Артем Блажиевский on 22.02.2022.
+//  Created by Артем Бумарь on 22.02.2022.
 //
+
 import UIKit
 
 class ViewController: UIViewController {
@@ -17,7 +18,7 @@ class ViewController: UIViewController {
     } ()
     
     private lazy var nameTextField: UITextField = {
-        let textField = UITextField()
+       let textField = UITextField()
         textField.placeholder = "введите имя..."
         textField.borderStyle = .roundedRect
         textField.backgroundColor = .systemBackground
@@ -27,15 +28,15 @@ class ViewController: UIViewController {
     
     private lazy var nameValidateButton: UIButton = {
         let button = UIButton(type: .system)
-        button.setTitle("Валидировать имя", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Валидировать", for: .normal)
         button.setTitle("Не дави на меня!", for: .highlighted)
         button.titleLabel?.font = UIFont.systemFont(ofSize: 17, weight: .bold)
         button.backgroundColor = .systemBlue
         button.setTitleColor(.white, for: .normal)
         button.layer.cornerRadius = 4
         button.addTarget(self,
-                         action: #selector(nameDidTapValidateButton), for: .touchUpInside)
-        button.translatesAutoresizingMaskIntoConstraints = false
+                         action: #selector(didTapValidateButton), for: .touchUpInside)
         return button
     }()
     
@@ -49,14 +50,14 @@ class ViewController: UIViewController {
     private lazy var emailCardView: UIView = {
         let view = UIView()
         view.backgroundColor = .secondarySystemBackground
-        view.layer.cornerRadius = 12
         view.translatesAutoresizingMaskIntoConstraints = false
+        view.layer.cornerRadius = 12
         return view
     } ()
     
     private lazy var emailTextField: UITextField = {
-        let textField = UITextField()
-        textField.placeholder = "введите почту..."
+       let textField = UITextField()
+        textField.placeholder = "введите имя..."
         textField.borderStyle = .roundedRect
         textField.backgroundColor = .systemBackground
         textField.translatesAutoresizingMaskIntoConstraints = false
@@ -65,13 +66,14 @@ class ViewController: UIViewController {
     
     private lazy var emailValidateButton: UIButton = {
         let button = UIButton(type: .system)
-        button.setTitle("Валидировать почту", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Валидировать", for: .normal)
         button.titleLabel?.font = UIFont.systemFont(ofSize: 17, weight: .bold)
         button.backgroundColor = .systemBlue
         button.setTitleColor(.white, for: .normal)
         button.layer.cornerRadius = 4
-        button.addTarget(self, action: #selector(emailDidTapValidateButton), for: .touchUpInside)
-        button.translatesAutoresizingMaskIntoConstraints = false
+        button.addTarget(self,
+                         action: #selector(didTapValidateButton), for: .touchUpInside)
         return button
     }()
     
@@ -82,26 +84,18 @@ class ViewController: UIViewController {
         return label
     }()
     
-    private lazy var authorHomeWorkLabel: UILabel = {
-        let label = UILabel()
-        label.text = "ДЗ Артема Блажиевского"
-        label.font = .systemFont(ofSize: 25)
-        label.translatesAutoresizingMaskIntoConstraints = false
-        return label
-    }()
+    
+    
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         configureView()
     }
     
-    let validationСheck = { (regExString: String, predicate: NSPredicate, whatToCheck: UITextField) -> Bool in
-        let regExString = regExString
-        let predicate = predicate
-        let isValid = predicate.evaluate(with: whatToCheck.text)
-        return isValid
-        }
-    }
+    
+}
 
 // MARK: - Private methods
 
@@ -119,15 +113,11 @@ extension ViewController {
         emailCardView.addSubview(emailValidateButton)
         emailCardView.addSubview(emailValidationResultLabel)
         
-        view.addSubview(authorHomeWorkLabel)
         
         NSLayoutConstraint.activate([
-            authorHomeWorkLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
-            authorHomeWorkLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            
-            nameCardView.topAnchor.constraint(equalTo: authorHomeWorkLabel.topAnchor, constant: 50),
-            nameCardView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
-            nameCardView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            nameCardView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 50),
+            nameCardView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            nameCardView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
             
             nameTextField.topAnchor.constraint(equalTo: nameCardView.topAnchor, constant: 16),
             nameTextField.leadingAnchor.constraint(equalTo: nameCardView.leadingAnchor, constant: 16),
@@ -140,9 +130,8 @@ extension ViewController {
             nameValidationResultLabel.topAnchor.constraint(equalTo: nameValidateButton.bottomAnchor, constant: 16),
             nameValidationResultLabel.leadingAnchor.constraint(equalTo: nameCardView.leadingAnchor, constant: 16),
             nameValidationResultLabel.trailingAnchor.constraint(equalTo: nameCardView.trailingAnchor, constant: -16),
-            nameValidationResultLabel.bottomAnchor.constraint(equalTo: nameCardView.bottomAnchor, constant: -16),
             
-            emailCardView.topAnchor.constraint(equalTo: nameCardView.bottomAnchor, constant: 50),
+            emailCardView.topAnchor.constraint(equalTo: nameCardView.bottomAnchor, constant: 200),
             emailCardView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
             emailCardView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
             
@@ -158,57 +147,20 @@ extension ViewController {
             emailValidationResultLabel.leadingAnchor.constraint(equalTo: emailCardView.leadingAnchor, constant: 16),
             emailValidationResultLabel.trailingAnchor.constraint(equalTo: emailCardView.trailingAnchor, constant: -16),
             emailValidationResultLabel.bottomAnchor.constraint(equalTo: emailCardView.bottomAnchor, constant: -16),
+            
         ])
     }
-    
+
     @objc
-    private func nameDidTapValidateButton() {
+    private func didTapValidateButton() {
         let regExString = "[A-Za-zА-ЯЁа-яё-]{2,}+\\s{1}+[A-Za-zА-ЯЁа-яё-]{2,}"
         let predicate = NSPredicate(format: "SELF MATCHES[c] %@", regExString)
         let isValid = predicate.evaluate(with: nameTextField.text)
-        
-        switch isValid {
-        case true:
-            nameValidationResultLabel.text = "Валидация прошла успешно"
-            nameValidationResultLabel.textColor = .systemGreen
-        case false:
-            nameValidationResultLabel.text = "В поле ошибка"
-            nameValidationResultLabel.textColor = .systemRed
-            
-            let animation = CABasicAnimation(keyPath: "position")
-            animation.duration = 0.07
-            animation.repeatCount = 4
-            animation.autoreverses = true
-            animation.fromValue = NSValue(cgPoint: CGPoint(x: nameCardView.center.x - 10, y: nameCardView.center.y))
-            animation.toValue = NSValue(cgPoint: CGPoint(x: nameCardView.center.x + 10, y: nameCardView.center.y))
-            nameCardView.layer.add(animation, forKey: "position")
-        }
-    }
-    
 
+        nameValidationResultLabel.text = isValid ? "Валидация прошла успешно" : "В поле ошибка"
+        nameValidationResultLabel.textColor = isValid ? .systemGreen : .systemRed
 
-    @objc
-    private func emailDidTapValidateButton() {
-        let regExString = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
-        let predicate = NSPredicate(format: "SELF MATCHES[c] %@", regExString)
-        let isValid = predicate.evaluate(with: emailTextField.text)
-        
-        switch isValid {
-        case true:
-            emailValidationResultLabel.text = "Валидация прошла успешно"
-            emailValidationResultLabel.textColor = .systemGreen
-        case false:
-            emailValidationResultLabel.text = "В поле ошибка"
-            emailValidationResultLabel.textColor = .systemRed
-            
-            let animation = CABasicAnimation(keyPath: "position")
-            animation.duration = 0.07
-            animation.repeatCount = 4
-            animation.autoreverses = true
-            animation.fromValue = NSValue(cgPoint: CGPoint(x: emailCardView.center.x - 10, y: emailCardView.center.y))
-            animation.toValue = NSValue(cgPoint: CGPoint(x: emailCardView.center.x + 10, y: emailCardView.center.y))
-            emailCardView.layer.add(animation, forKey: "position")
-        }
     }
+
 }
 
