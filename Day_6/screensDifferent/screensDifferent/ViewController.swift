e3738d6810add28cb693d094468b9aff02eae839
blazhievsky <blazhievsky007@ya.ru>
2022-03-09 10:10:58 +0300

    Day_6

---
+++ Day_6/screensDifferent/screensDifferent/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  screensDifferent
+//
+//  Created by Artem Blazhievsky on 09.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
